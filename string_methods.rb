7a330a3298736106f67abc000db0c31bd3eb37e6
SnaxForFree <52750277+SnaxForFree@users.noreply.github.com>
2019-07-10 10:16:40 -0400

    Add files via upload

---
+++ string_methods.rb
@@ -0,0 +1,13 @@
+
+puts "programming".upcase()
+puts "programming".downcase()
+phrase = "    Giraffe Academy    "
+puts phrase.strip()
+phrase = "Giraffe Academy"
+puts phrase.length()
+puts phrase.include? "Academy"
+phrase = "Giraffe Academy"
+puts phrase [0]
+puts phrase [0,5]
+puts phrase.index("G")
+puts phrase.index("r")
---
+++ types of data that can be stored.rb
@@ -0,0 +1,7 @@
+
+name = "Xander"
+age = 16
+gpa = 3.67
+ismale = true
+istall = false
+flaws = nil
